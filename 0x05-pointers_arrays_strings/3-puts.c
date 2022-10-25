cbe9f72865eee805f12715a515432cafd838be7a
“IFrakas” <s.franciskamau@gmail.com>
2022-10-25 12:53:12 -0700

    newlinw

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,17 +1,14 @@
 #include "main.h"
 /**
- * _puts - prints a string, followed by a new line, to stdout
- * @str: pointer to the string to print
- * Return: void
+ *_puts - prints a string, followed by a new line,
+ *@str: pointer to the string to print
+ *Return: void
  */
 void _puts(char *str)
 {
-	int i = 0;
-
-	for (i = 0; *(str + i) != '\0'; i++)
+	while (*str != '\0')
 	{
-		_putchar(*(str + i));
-		i++;
+		_putchar(*str++);
 	}
 	_putchar('\n');
 }

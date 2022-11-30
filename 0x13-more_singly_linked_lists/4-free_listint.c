039237ed854ce68c76c622199792d9dc7a140863
IFrakas <s.franciskamau@gmail.com>
2022-11-30 10:42:40 -0800

    frees a listint_t list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * free_listint - frees the list mem
+ *  @head: head of struct
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		free(head);
+		head = tmp->next;
+	}
+}
